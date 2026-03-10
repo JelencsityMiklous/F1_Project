5a3a35fcd54650ccdc2822da8ae0c6eb96c7bcd8
JelencsityMiklous <jelencsitymiklos@turr.hu>
2026-03-10 13:23:07 +0100

    Aaaa

--- Backend/src/services/race.service.ts
+++ Backend/src/services/race.service.ts
@@ -1,14 +1,10 @@
 import { Repository } from "typeorm";
 import { AppDataSource } from "../data-source";
-import { Driver } from "../entities/driver.entity";
-import { Team } from "../entities/team.entity";
 import { Circuit } from "../entities/circuit.entity";
 import { Race } from "../entities/race.entity";
 
 export class RaceService {
     constructor(
-        private driverRepo: Repository<Driver> = AppDataSource.getRepository("Driver"),
-        private teamRepo: Repository<Team> = AppDataSource.getRepository("Team"),
         private circuitRepo: Repository<Circuit> = AppDataSource.getRepository("Circuit"),
         private raceRepo: Repository<Race> = AppDataSource.getRepository("Race"),
     ) {}
@@ -22,7 +18,7 @@ export class RaceService {
     }
 
     async listRaces() {
-        return this.raceRepo.find({ relations: ["drivers", "circuit"] });
+        return this.raceRepo.find({ relations: ["circuit"] });
     }
 
     async getRaceById(id: number) {
